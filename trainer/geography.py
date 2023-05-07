ee626fa17f280bb134c1e0ee6fe23bc21c987e34
YasuoKatou <yasuokatou@gmail.com>
2023-05-07 19:00:37 +0900

    地理データに日本の７地方を追加

--- trainer/geography.py
+++ trainer/geography.py
@@ -21,7 +21,7 @@ class Geography(Trainer):
         '福岡':'', '佐賀':'', '長崎':'', '熊本':'', '大分':'', '宮崎':'', '鹿児島':'', '沖縄':'那覇',
     }
 
-    def add_jp_pref(self):
+    def add_jp_pref(self, categories=[]):
         l = []
         for p, c in self._JP_PREF.items():
             c_sufix = '市'
@@ -37,11 +37,34 @@ class Geography(Trainer):
             l.append([
                 '{}{}の県庁所在地は？'.format(p, p_sufix),
                 '{}{}です。'.format(c if c else p, c_sufix)])
-        self.append_yml(self._OUT_FILE, l, ['地理'])
+        self.append_yml(self._OUT_FILE, l, categories)
         print('{}都道府県の県庁所在地を追加'.format(len(l)))
 
-if __name__ == '__main__':
-    geography = Geography()
-    geography.add_jp_pref()
+    _JP_7AREA = {
+        '北海道': ['北海道'],
+        '東北': ['青森県', '岩手県', '秋田県', '宮城県', '山形県', '福島県'],
+        '関東': ['茨城県', '栃木県', '群馬県', '埼玉県', '千葉県', '東京都', '神奈川県'],
+        '中部': ['新潟県', '富山県', '石川県', '福井県', '山梨県', '長野県', '岐阜県', '静岡県', '愛知県'],
+        '近畿': ['三重県', '滋賀県', '京都府', '大阪府', '兵庫県', '奈良県', '和歌山県'],
+        '中国四国': ['鳥取県', '島根県', '岡山県', '広島県', '山口県', '徳島県', '香川県', '愛媛県', '高知県'],
+        '九州沖縄': ['福岡県', '佐賀県', '長崎県', '熊本県', '大分県', '宮崎県', '鹿児島県', '沖縄県']
+    }
 
+    def add_jp_7group(self, categories=[]):
+        l = []
+        for an, ps in self._JP_7AREA.items():
+            #print('\t{}地方 : {}'.format(an, ','.join(ps)))
+            l.append([
+                '{}地方は？'.format(an),
+                '{}です。'.format(' '.join(ps))])
+        self.append_yml(self._OUT_FILE, l, categories)
+        print('{}地方を追加'.format(len(l)))
+
+if __name__ == '__main__':
+    # 地理データを作成
+    geography = Geography()
+    # 47都道府県
+    geography.add_jp_pref(['地理'])
+    # ７分割地方の都道府県
+    geography.add_jp_7group()
 #[EOF]
--- trainer/trainer.py
+++ trainer/trainer.py
@@ -12,10 +12,12 @@ class Trainer:
     def append_yml(self, file_name, qa_list, categories):
         p = self.outFile(file_name)
         with p.open(mode='a', encoding='utf-8') as f:
-            f.write('categories:\n')
+            if categories:
+                f.write('categories:\n')
             for category in categories:
                 f.write('- {}\n'.format(category))
-            f.write('conversations:\n')
+            if categories:
+                f.write('conversations:\n')
             for items in qa_list:
                 f.write('- - {}\n'.format(items[0]))
                 f.write('  - {}\n'.format(items[1]))
